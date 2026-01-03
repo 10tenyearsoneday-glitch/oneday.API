da97f3d65923c4baeaa4c73a2d6cfaee1666ff91
10tenyearsoneday-glitch <10tenyears.oneday@gmail.com>
2026-01-04 03:00:44 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("tenyears oneday api is running");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
 
